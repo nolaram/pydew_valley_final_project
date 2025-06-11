fab27e3e0b506b818558235c7708b30216be8b46
nolaram <marloncopino10@gmail.com>
2025-06-11 16:49:51 +0800

    add_audio

--- level.py
+++ level.py
@@ -38,9 +38,14 @@ class Level:
 		self.menu = Menu(self.player, self.toggle_shop)
 		self.shop_active = False
 
+		# music
+		self.success = pygame.mixer.Sound('audio/success.wav')
+		self.success.set_volume(0.3)
+		self.music = pygame.mixer.Sound('audio/music.mp3')
+		self.music.play(loops = -1)
 
 	def setup(self):
-		tmx_data = load_pygame('data/map.tmx')
+		tmx_data = load_pygame('../data/map.tmx')
 
 		# house 
 		for layer in ['HouseFloor', 'HouseFurnitureBottom']:
@@ -105,6 +110,7 @@ class Level:
 	def player_add(self,item):
 
 		self.player.item_inventory[item] += 1
+		self.success.play()
 
 	def toggle_shop(self):
 
